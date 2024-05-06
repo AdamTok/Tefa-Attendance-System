f3f300d6810ff72624f2c588ac3d820e13a2d040
Adam Darmawansa <adamdarmawansa439@gmail.com>
2024-05-06 21:20:30 +0700

    Fixing code
    Perbaikan kode program.

--- facetrainer.py
+++ facetrainer.py
@@ -64,11 +64,12 @@ base_path = 'Datasets_User'  # Path dasar untuk dataset wajah anggota
 # Fungsi untuk membaca foto-foto wajah, mengonversi ke skala abu-abu, dan mengembalikan sampel-sampel wajah dan label-labelnya
 def getImagesAndLabels(path):
     faceSamples = []
-    ids = []
+    ids = {}
+    id_count = 0
 
     for file_name in os.listdir(path):
         if file_name.endswith(".jpg"):
-            id = int(file_name.split(".")[0])  # Mendapatkan ID dari nama file
+            id_count += 1
             img_path = os.path.join(path, file_name)
             img = cv2.imread(img_path, cv2.IMREAD_GRAYSCALE)
 
@@ -76,13 +77,14 @@ def getImagesAndLabels(path):
 
             for (x, y, w, h) in faces:
                 faceSamples.append(img[y:y+h, x:x+w])
-                ids.append(id)
+                ids[file_name] = id_count
 
     return faceSamples, ids
 
 # Fungsi untuk melatih recognizer menggunakan sampel-sampel wajah dan label-labelnya
 def trainRecognizer(faces, ids, trained_path): 
-    recognizer.train(faces, np.array(ids))
+    face_ids = [ids[key] for key in ids]
+    recognizer.train(faces, np.array(face_ids))
     # Membuat direktori untuk menyimpan model yang dilatih
     if not os.path.exists(trained_path):
         os.makedirs(trained_path)
@@ -101,7 +103,7 @@ for nim_folder in os.listdir(base_path):
         # Melatih recognizer menggunakan sampel-sampel wajah dan label-labelnya
         trainRecognizer(faces, ids, trained_folder_path)
         # Mencetak jumlah wajah unik yang telah dilatih untuk setiap folder
-        num_faces_trained = len(set(ids))
+        num_faces_trained = len(set(ids.values()))
         print("\n [INFO] {} wajah dari folder {} telah dilatih dan disimpan di {}.".format(num_faces_trained, nim_folder, trained_folder_path))
 
 print("\n Training selesai.")
